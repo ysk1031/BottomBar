dbad9bb33df368ea0ed580df4c989668665f7065
Tushar Acharya <tushar-acharya@users.noreply.github.com>
2017-03-25 03:11:21 +0530

    Data binding fixes clean (#712)
    * Fix Bottom Bar issues with Android Data Binding
    
    * keep .idea
    
    * travis changes

--- bottom-bar/src/main/java/com/roughike/bottombar/BottomBarTab.java
+++ bottom-bar/src/main/java/com/roughike/bottombar/BottomBarTab.java
@@ -142,7 +142,7 @@ public class BottomBarTab extends LinearLayout {
             titleView.setTextAppearance(getContext(), titleTextAppearanceResId);
         }
 
-        titleView.setTag(titleTextAppearanceResId);
+        titleView.setTag(R.id.bb_bottom_bar_appearance_id, titleTextAppearanceResId);
     }
 
     private void updateCustomTypeface() {
@@ -265,10 +265,10 @@ public class BottomBarTab extends LinearLayout {
     }
 
     int getCurrentDisplayedIconColor() {
-        Object tag = iconView.getTag();
+        Object tag = iconView.getTag(R.id.bb_bottom_bar_color_id);
 
         if (tag instanceof Integer) {
-            return (int) iconView.getTag();
+            return (int) tag;
         }
 
         return 0;
@@ -283,10 +283,10 @@ public class BottomBarTab extends LinearLayout {
     }
 
     int getCurrentDisplayedTextAppearance() {
-        Object tag = titleView.getTag();
+        Object tag = titleView.getTag(R.id.bb_bottom_bar_appearance_id);
 
         if (titleView != null && tag instanceof Integer) {
-            return (int) titleView.getTag();
+            return (int) tag;
         }
 
         return 0;
@@ -422,7 +422,7 @@ public class BottomBarTab extends LinearLayout {
     private void setColors(int color) {
         if (iconView != null) {
             iconView.setColorFilter(color);
-            iconView.setTag(color);
+            iconView.setTag(R.id.bb_bottom_bar_color_id, color);
         }
 
         if (titleView != null) {
